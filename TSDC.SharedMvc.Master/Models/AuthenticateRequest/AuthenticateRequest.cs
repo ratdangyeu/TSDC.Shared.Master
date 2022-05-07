7741e5b7d8ae79ed6dd5258c307848e358292a7c
thaind <thai.nd@yahoo.com>
2022-05-07 17:15:25 +0700

    up

---
+++ TSDC.SharedMvc.Master/Models/AuthenticateRequest/AuthenticateRequest.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace TSDC.SharedMvc.Master.Models
+{
+    public class AuthenticateRequest
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class AuthenticateRequestValidator : AbstractValidator<AuthenticateRequest>
+    {
+        public AuthenticateRequestValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty()
+                .WithMessage("Tên đăng nhập không được để trống");
+
+            RuleFor(x => x.Password).NotEmpty()
+                .WithMessage("Mật khẩu không được để trống");
+        }
+    }
+}
